e9d30a0d8760e1c87fbb4606b19bedc1afbb8c16
WarmColor <admin@warmcolor.net>
2016-05-16 17:21:37 +0800

    init

---
+++ app/src/main/java/net/warmcolor/blog/download/HttpDownloader.java
@@ -0,0 +1,4 @@
+package net.warmcolor.blog.download;
+
+public class HttpDownloader {
+}
---
+++ app/src/main/java/net/warmcolor/blog/mp3player/Mp3ListActivity.java
@@ -0,0 +1,13 @@
+package net.warmcolor.blog.mp3player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mp3ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mp3_list);
+    }
+}
